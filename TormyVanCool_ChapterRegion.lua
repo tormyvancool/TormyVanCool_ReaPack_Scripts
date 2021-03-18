c83c678c6bfa38789026614fca1c783d449f4a5b
TormyVC <tormy.vancool@gmail.com>
2021-03-18 13:08:32 +0100

    .

--- TormyVanCool_ChapterRegion.lua
+++ TormyVanCool_ChapterRegion.lua
@@ -1,7 +1,7 @@
 --[[
 @description Chapter region for podcasts and recorded broadcasts
 @author Tormy Van Cool
-@version 2.7
+@version 2.7.1
 @screenshot Example: ChapterRegion.lua in action https://github.com/tormyvancool/TormyVanCool_ReaPack_Scripts/Region.gif
 @changelog:
 v1.0 (01 feb 2021)
@@ -68,7 +68,8 @@ v2.7
     999,1,"PODCAST_TITLE > TORMY VAN COOL | End  Part 1 >"
     In case the Podcast is not splitted in parts, it returns
     999,1,"PODCAST_TITLE > TORMY VAN COOL | The  End >"
-    
+v2.7.1
+  + Added Instructions table
 @about
 # Chapter Region for Podcasts and Recorded Broadcasts
   It's an ideal feature for Podcasts and Recorded Broadcasts
@@ -276,8 +277,10 @@ itemduration = roundup
 --------------------------------------------------------------------
 -- Asks for user's inputs
 --------------------------------------------------------------------
+reaper.ShowConsoleMsg("")
+reaper.ShowConsoleMsg("INSTRUCTIONS\n\n\nSONG TITLE and PERFORMER are mandatory fields.\n\nPRODUCTION YEAR and LABEL(s) [Optional fields] check on:\nhttps://www.discogs.com or\nhttps://www.musicbrainz.org\n\nYou can select the links above, copy them and paste into the browser\n\nPRODUCTION LABELS(s):\nin case of multiple labels, separate them by commas")
 repeat
-retval, InputString=reaper.GetUserInputs("PODCAST/BROADCAST: SONG DATA", 4, "Song Title (Mandatory),separator=\n,extrawidth=400,Performer (Mandatory),Production Year,Label", SongTitle..LF..SongPerformer..LF..SongYear..LF..SongLabel)
+retval, InputString=reaper.GetUserInputs("PODCAST/BROADCAST: SONG DATA", 4, "Song Title (Mandatory),separator=\n,extrawidth=400,Performer (Mandatory),Production Year,Label(s) (separated by commas)", SongTitle..LF..SongPerformer..LF..SongYear..LF..SongLabel)
 InputString = ChapRid(ChapRid(ChapRid(ChapRid(ChapRid(ChapRid(InputString, pipe), '-'), ':'), '='), '"'), '|') -- No reserved characters can be written
 if retval==false then return end
 if retval then
