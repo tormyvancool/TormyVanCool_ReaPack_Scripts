644fab90caa2de2a90971c7262a384464ac282c5
TormyVC <tormy.vancool@gmail.com>
2021-05-31 01:56:36 +0200

    .

--- TormyVanCool_VSTs-VSTis-Extractor.lua
+++ TormyVanCool_VSTs-VSTis-Extractor.lua
@@ -1,22 +1,25 @@
 --[[
 @description Extracts and exports VSTs and VSTIs from reaper-vstplugins64.ini, in HTML and CSV format on a Project Folder
 @author Tormy Van Cool
-@version 1.0
+@version 1.1
 @screenshot
 @changelog:
 v1.0 (30 may 2021)
   + Initial release
+v1.1 (31 may 2021)
+  + Save status of HTML File
+  + Retrieve status of HTML file
 ]]
 reaper.ShowConsoleMsg('')
-local version = "1.0"
+local version = "1.1"
 local proj_path = reaper.GetProjectPathEx(0 , '' ):gsub("(.*)\\.*$","%1")
 local path = reaper.GetResourcePath()..'\\reaper-vstplugins64.ini'
 local FileName = proj_path.."/REAPER.VSTinstalled"
 local VST_RegisterHTML = FileName..".html"
 local VST_RegisterCSV = FileName..".csv"
-local VST_RegisterJSN = FileName..".json"
+local VST_RegisterJSN = "REAPER.VSTinstalled.json"
 local date = os.date("%Y-%m-%d %H:%M:%S")
-VST_RegisterJSN = VST_RegisterJSN:gsub( "\\", "/")
+local VST_RegisterJSN = VST_RegisterJSN:gsub( "\\", "/")
 
 local HeaderHTML = [[
 <!doctype html>
@@ -27,9 +30,11 @@ local HeaderHTML = [[
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Arimo&display=swap" rel="stylesheet">
     <style>
-      th, td {font-family: 'Arimo', sans-serif;}
+      th, td, span {font-family: 'Arimo', sans-serif;}
       .table_header{background: #0057a1 !important; color: white;}
-      table{margin-bottom: 12px; width:70%}
+      form {width: 70%}
+      table{margin-bottom: 12px;}
+      #title, #rows {width: 1320px;}
       th.header { background: #2db1ef; color: white; font-size: 51px; position: relative; padding: 30px; }
       .table_title { margin-top: 10px; background: linear-gradient( 48deg , #2dbbff, transparent); font-weight: bolder; color: #ca5603; font-size: 22px; }
       tr:nth-child(even) td.solo {background: #ffc107;text-align: center; }   
@@ -53,114 +58,95 @@ local HeaderHTML = [[
       sub { font-size: 12px; float: right; position: absolute; bottom: 10px; right: 10px; }
       .spacer{width: 100%;height:50px}
       .right{text-align: right;}
+      button.file-save, button.file-load, button.reset { position: absolute; width: 133px; height: 50px; left: 31px; }
+      button.file-save { top: 20px; }
+      button.file-load { top: 75px; }
+      button.reset { top: 130px; }
+      .checkbox {    width: 15px;    padding: 0 28px;}
+      .name {    width: 770px;}
+      .file {    width: 390px;}
+      div#buttons { position: fixed; left: 60px; top: 150px; background: #0057a1; width: 200px; height: 200px; border-radius: 0 0 10px 10px; }
+      span#info { background: red; position: fixed; left: 60px; top: 11px; width: 180px; border-radius: 10px 10px 0 0; height: 119px; padding: 10px; text-align: center; color: white; font-weight: bold; }
+      table#rows { position: relative; top: 153px; }
+      table#title { position: fixed; top: 0px; left: 291px; z-index: 1; }
     </style>
     <script>
-    //
-    // jQuery Plugin
-    //
-    ;(function($) {
-        $.fn.toJSON = function() {
-            var $elements = {};
-            var $form = $(this);
-            $form.find('input, select, textarea').each(function(){
-              var name = $(this).attr('name')
-              var type = $(this).attr('type')
-              if(name){
-                var $value;
-                if(type == 'radio'){
-                  $value = $('input[name='+name+']:checked', $form).val()
-                } else if(type == 'checkbox'){
-                  $value = $(this).is(':checked')
-                } else {
-                  $value = $(this).val()
-                }
-                $elements[$(this).attr('name')] = $value
-              }
-            });
-            return JSON.stringify( $elements )
-        };
-        $.fn.fromJSON = function(json_string) {
-            var $form = $(this)
-            var data = JSON.parse(json_string)
-            $.each(data, function(key, value) {
-              var $elem = $('[name="'+key+'"]', $form)
-              var type = $elem.first().attr('type')
-              if(type == 'radio'){
-                $('[name="'+key+'"][value="'+value+'"]').prop('checked', true)
-              } else if(type == 'checkbox' && (value == true || value == 'true')){
-                $('[name="'+key+'"]').prop('checked', true)
-              } else {
-                $elem.val(value)
-              }
-            })
-        };
-    }( jQuery ));
-    
-    //
-    // DEMO CODE
-    // 
-    $(document).ready(function(){
-       $("#_save").on('click', function(){
-         console.log("Saving form data...")
-         var data = $("form#myForm").toJSON()
-         console.log(data);
-         //localStorage['form_data'] = data;
-         function saveText(text, filename){
-           var a = document.createElement('a');
-           a.setAttribute('href', 'data:text/plain;charset=utf-8,'+encodeURIComponent(text));
-           a.setAttribute('download', filename);
-           a.click()
-         }
-         saveText( data, "REAPER.VSTinstalled.json" );
 
-         
-         return false;
-       })
-       
-       $("#_load").on('click', function(){
-         if(localStorage['form_data']){
-           console.log("Loading form data...")
-           //console.log(JSON.parse(localStorage['form_data']))
-           //console.log(JSON.parse('file:///]]..VST_RegisterJSN..[['))
-           
+      $(document).ready(function(){
 
-           $("form#myForm").fromJSON($.getJSON("file:///]]..VST_RegisterJSN..[["))
-         } else {
-           console.log("Error: Save some data first")
-         }
-         
-         return false;
-       })
+        //prevent the form from submitting on button click
+      $('form button').on('click', e => e.preventDefault());
+      //open file loader
+      $('button.file-load').on('click', function() {
+        $('.file-loader').click();
+      });
+      
+      //uncheck all checkboxes
+      $('button.reset').on('click', function() {
+        $('table input[type="checkbox"]').each(function() {
+          this.checked = false;
+        });
+      });
+      
+      //convert checkbox states to json and save
+      $('button.file-save').on('click', function() {
+        let a = document.createElement('a'),
+            states = {};
+            
+        $('table input[type="checkbox"]').each(function() {
+          states[this.name] = this.checked
+        });
+            
+        a.setAttribute('href', 'data:text/plain;charset=utf-8,' + JSON.stringify(states));
+        a.setAttribute('download', ']]..VST_RegisterJSN..[[');
+        a.click();
+        a.remove();
+      });
+      
+      //load saved states
+      $('.file-loader').on('change', function() {
+        let reader = new FileReader();
+      
+        reader.onload = function(file) {
+          $.each(JSON.parse(file.target.result), function(id, state) {
+            $(`form input[name="${id}"]`).prop('checked', state);
+          });
+        };
+      
+        reader.readAsText(this.files[0]);
+      });
+
     });
     </script>
   </head>
   <body>
-  <!--
-  <form  class="center" action="#" method="get" id="myForm">
-  <button id="_save">Save</button>
-  <button id="_load">Load</button>
-  <input type="reset">
-  -->
-    <table class="center">
+    <table id="title" class="center">
       <thead>
         <tr>
-          <th colspan="2" class="header">REAPER VST/VSTi INSTALLED EXTRACTOR<sub>]]..date..[[ - v.]]..version..[[ by Tormy Van Cool</sub></th>
+          <th colspan="3" class="header">REAPER VST/VSTi INSTALLED EXTRACTOR<sub>]]..date..[[ - v.]]..version..[[ by Tormy Van Cool</sub></th>
         <tr>
-          <!-- <th class="table_header">&nbsp;</th> -->
-          <th class="table_header">VST/VSTi NAME (Manufacturer)</th>
-          <th class="table_header">Related FILE</th>
+          <th class="table_header checkbox">&nbsp;</th>
+          <th class="table_header name">VST/VSTi NAME (Manufacturer)</th>
+          <th class="table_header file">Related FILE</th>
         </tr>
       </thead>
-      <tdoby>
+    </table>
+ <form  class="center" action="#" method="get" id="TableForm">
+  <div id="buttons">
+    <button class="file-save">Save</button>
+    <button class="file-load">Load</button>
+    <!-- if <input type="file"> isn't hidden, it shows the current file selected -->
+    <input class="file-loader" type="file" hidden>
+    <button class="reset">Reset</button>
+  </div>
+    <span id="info" class="centertext">IMPORTANT</br></br>You MUST HAVE internet connection</br>to make correctly</br>work this page!!!</span>
+    <table id="rows" class="center">
+
+      <tbody>
 ]]
 local FooterHTML = [[
       </tbody> 
     </table>
-    <!--
-    <button id="_save">Save</button>
-    <button id="_load">Load</button>
-    <input type="reset">
-    -->
     </form>
   </body>
 </html>]]
@@ -182,7 +168,7 @@ function main()
   CSV:write(HeaderCSV)
   local lineHTML = ''
   local lineCSV = ''
-  local n = 0
+  local n = 1
   for _ in handle:lines() do
     s = handle:read("*l")
 
@@ -197,7 +183,8 @@ function main()
 
         if VST_Name ~= nil and VST_dll ~= nil then
           VST_Name = string.gsub(VST_Name, "%,", " / ")
-          lineHTML = lineHTML..'<tr><!--<td><input type="checkbox" name="checkfield'..n..'"></td>--><td class="name">'..VST_Name..'</td><td>'..VST_dll..'</td></tr>'
+          
+          lineHTML = lineHTML..'<tr><td><input type="checkbox" name="checkfield'..n..'" class="checkbox"></td><td class="name">'..VST_Name..'</td><td class="file">'..VST_dll..'</td></tr>'
           lineCSV = lineCSV..','..VST_Name..','..VST_dll..'\n' 
           n=n+1
         end
