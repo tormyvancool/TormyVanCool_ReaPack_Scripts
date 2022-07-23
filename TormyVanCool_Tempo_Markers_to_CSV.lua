63764a095e6a7efd119124e31612b9ab6c4aa869
Tormy Van Cool <32305535+tormyvancool@users.noreply.github.com>
2022-07-23 23:34:05 +0200

    .

--- TormyVanCool_Tempo_Markers_to_CSV.lua
+++ TormyVanCool_Tempo_Markers_to_CSV.lua
@@ -1,25 +1,38 @@
 --[[
 @description CSV containing Time Markers
 @author Tormy Van Cool
-@version 1.1
+@version 1.2
 @screenshot
 @changelog:
-v1.0 (23 july 222)
+v1.0 (23 july 2022)
   + Initial release
-v1.0 (23 july 222)
+v1.1 (23 july 2022)
   # Time in format HH:mm:ss
+v1.2 (23 july 2022)
+  # Exports with predetermined and univocal file_name
+  - Time in format HH:mm:ss
+  + Time in format HH:mm:ss in console
+  + Line number
+  + Fractional Value
+  + Info Pop Up
+  - Commas
+  + Tabs
+  + Beat
 ]]
 
 ---------------------------------------------
 -- VARIABLES
 ---------------------------------------------
-local version = "1.1"
+local version = "1.2"
 local extension = ".csv"
+local LF = '\n'
+local tab = '\t'
 local howmany = reaper.CountTempoTimeSigMarkers(0) -- counts markers QTY
 local pj_name_ = reaper.GetProjectName(0, "") -- gets project name
-local pj_name = pj_name_.. extension
+local pj_name = "Tempo_Markers".. extension
 local pj_path = reaper.GetProjectPathEx(0 , '' ):gsub("(.*)\\.*$","%1") -- gets project path
 local count = 0
+local ConsMsg = ''
 
 
 ---------------------------------------------
@@ -30,8 +43,9 @@ CSV_file = io.open(pj_path..'\\'..pj_name, "w")
 ---------------------------------------------
 -- CSV HEADER
 ---------------------------------------------
-local CSV_header = 'TIME MARKERS WRAP UP version'..version..'\nby Tormy VAN COOL\n\n\nBPM,TIME POSITION,MEASURE POSITION,BEAT POSITION,TIME SIGNATURE'
+local CSV_header = 'TEMPO MARKERS WRAP UP version '..version.. LF ..'by Tormy VAN COOL'
 CSV_file:write(CSV_header)
+
 
 ---------------------------------------------
 -- FUNCTIONS
@@ -41,9 +55,9 @@ function SecondsToClock(seconds) -- Turns seconds into the format: "hh:mm:ss"
   if seconds <= 0 then
     return "00:00:00";
   else
-    hours = string.format("%02.f", math.floor(seconds/3600));
-    mins = string.format("%02.f", math.floor(seconds/60 - (hours*60)));
-    secs = string.format("%02.f", math.floor(seconds - hours*3600 - mins *60));
+    local hours = string.format("%02.f", math.floor(seconds/3600));
+    local mins = string.format("%02.f", math.floor(seconds/60 - (hours*60)));
+    local secs = string.format("%02.f", math.floor(seconds - hours*3600 - mins *60));
     return hours..":"..mins..":"..secs
   end
 end
@@ -52,8 +66,18 @@ end
 -- TEMPO MARKERS PROBING
 ---------------------------------------------
 while count < howmany do
-  local retval, timepos, measurepos, beatpos, bpm, timesig_num = reaper.GetTempoTimeSigMarker(0, count) -- Extract markers infos
-  csv = '\n' .. bpm .. ',' .. SecondsToClock(timepos) .. ',' .. SecondsToClock(measurepos) .. ',' .. SecondsToClock(beatpos) .. ',' .. timesig_num
+  local retval, timepos, measurepos, beatpos, bpm, timesig_num, timesig_denom, lineartempo = reaper.GetTempoTimeSigMarker(0, count) -- Extract markers infos
+  SampleQTY = reaper.format_timestr_pos( timepos, "", 4 )
+  Beat = reaper.format_timestr_pos( timepos, "", 2 )
+  csv =  count .. tab .. bpm .. tab .. timepos .. tab .. measurepos .. tab .. beatpos .. tab .. SampleQTY .. tab .. timesig_num ..'\\'.. timesig_denom .. LF
+  ConsMsg = ConsMsg .. 'N: '.. count .. LF ..
+            'BPM: ' .. bpm .. LF ..
+            'Time Position: ' .. SecondsToClock(timepos) .. LF ..
+            'Measure Position: ' .. SecondsToClock(measurepos) .. LF ..
+            'Beat: ' .. Beat .. LF .. 
+            'Beat Position: ' .. SecondsToClock(beatpos) .. LF ..
+            'Samples: ' .. SampleQTY .. LF ..
+            'Fractional: ' .. timesig_num ..'\\'.. timesig_denom .. LF .. LF
   CSV_file:write(csv)
   count = count +1
 end
@@ -62,3 +86,6 @@ end
 -- FILE CLOSURE
 ---------------------------------------------
 CSV_file:close()
+reaper.MB("File Path: " .. pj_path .. "Saved" ..LF .. "CSV File: "  .. pj_name  , "Saved CSV" ,0)
+reaper.ShowConsoleMsg('')
+reaper.ShowConsoleMsg(CSV_header .. LF .. LF .. ConsMsg)
