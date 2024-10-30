313a0682f0a8196b6b9dce39a6f502a39035479c
Tormy Van Cool <32305535+tormyvancool@users.noreply.github.com>
2024-10-30 22:38:47 +0100

    .

--- TOST_MAIN.lua
+++ TOST_MAIN.lua
@@ -1,1 +1,8 @@
--- @noindex
+-- @description Youtube Downloader
+-- @author Tormy Van Cool
+-- @version 1.0
+-- @provides
+--   [data] yt-dlp/yt-dlp.exe https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe
+--   [data] yt-dlp/ytdlp_linux https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp
+--   [data] yt-dlp/ytdlp_macos https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_macos
+reaper.ShowConsoleMsg("hello")
