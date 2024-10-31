8a3a74c7104b96e1014afc7118c74ef2c847e075
Tormy Van Cool <32305535+tormyvancool@users.noreply.github.com>
2024-10-31 22:21:59 +0100

    .

--- Various/TOST_MAIN.lua
+++ Various/TOST_MAIN.lua
@@ -2,7 +2,7 @@
 -- @author Tormy Van Cool
 -- @version 1.0
 -- @provides
---   [windows] AAA/yt-dlp.exe https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe
---   [linux] AAA/ytdlp_linux https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp
---   [darwin] AAA/ytdlp_macos https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_macos
+--   [data windows] AAA/yt-dlp.exe https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe
+--   [data linux] AAA/ytdlp_linux https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp
+--   [data darwin] AAA/ytdlp_macos https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_macos
 reaper.ShowConsoleMsg("hello")
