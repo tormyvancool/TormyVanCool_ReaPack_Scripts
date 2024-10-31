084b536efc151a8ef73cf50f14a53c33752a3c8c
Tormy Van Cool <32305535+tormyvancool@users.noreply.github.com>
2024-10-31 18:44:33 +0100

    .

--- TOST_MAIN.lua
+++ TOST_MAIN.lua
@@ -2,8 +2,7 @@
 -- @author Tormy Van Cool
 -- @version 1.0
 -- @provides
---   [data] yt-dlp/yt-dlp.exe https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe
---   [data] porcodio/this.exe https://github.com/tormyvancool/TormyVanCool_ReaPack_Scripts/blob/master/reaper726_x64-install.exe
---   [data] yt-dlp/ytdlp_linux https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp
---   [data] yt-dlp/ytdlp_macos https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_macos
+--   [data windows] yt-dlp/yt-dlp.exe https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe
+--   [data linux] yt-dlp/ytdlp_linux https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp
+--   [data darwin] yt-dlp/ytdlp_macos https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_macos
 reaper.ShowConsoleMsg("hello")
